380d64942972d11e079a8f3b8660f5b5b2775d12
ParkSungHo007 <gr347034@graded.br>
2019-10-22 19:57:16 -0300

    Finally got one of the array3 done and simplified

--- src/data/arraySolutions.js
+++ src/data/arraySolutions.js
@@ -894,66 +894,33 @@ solutions.seriesUp = function seriesUp(n) {
 }
 
 solutions.maxMirror = function maxMirror(nums) {
-
-  function matchesForN(nums, n) {
-    // initially lets solve for n=3, then we can generalise to any n
-    // step1 get the candidates, which is [1,2,3] and [2,3,4] etc
-    let candidates = [];
-
-    for (let i = 0; i < nums.length - (n - 1); i++) {
-      let candidate = nums.slice(i, i + n);
-      candidates.push(candidate);
-    }
-
-    //step2. reverse nums. Since reverse() alters the array, prevent that by saving
-    // reversed array in a variable.
-    let rev_nums = nums.slice().reverse();
-
-    let comparisons = [];
-
-    //step3. make the copmarison arrays. like in candidates but reversed.
-    for (let i = 0; i < rev_nums.length - (n - 1); i++) {
-      let comparison = rev_nums.slice(i, i + n);
-      comparisons.push(comparison);
-    }
-
-    let matches = []
-    for (let candidate of candidates) {
-      for (let comparison of comparisons) {
-        match = isEqual(candidate, comparison.slice())
-        if (match) {
-          matches.push(candidate);
-          break;
-        }
-      }
-    }
-
-    return matches;
-
+//This solution was made by Sung Ho Park. He took 5 hours to simplify the solution. You're welcome. 
+  //.includes() method checks whether a specific string exists within an string or not.toString() converts a number to a string 
+  if(nums.length ==0){
+    return 0; 
   }
-
-  //function we call to check if the sub-arrays are equal
-  function isEqual(array1, array) {
-    for (let i = 0, l = array1.length; i < l; i++) {
-      // Check if we have nested arrays
-      if (array1[i] instanceof Array && array[i] instanceof Array) {
-        // recurse into the nested arrays
-        if (!array1[i].equals(array[i]))
-          return false;
-      }
-      else if (array1[i] != array[i]) {
-        // Warning - two different object instances will never be equal: {x:20} != {x:20}
-        return false;
-      }
-    }
-    return true;
+  //reverses nums and makes it into a string 
+  let reverse= "";
+  reverse = reverse + nums[nums.length-1]; 
+  for(let i=nums.length-2; i>=0; i--){
+    reverse = reverse + "," + nums[i].toString(); 
   }
-
-  for (let n = nums.length; n >= 0; n--) {
-    let matches = matchesForN(nums, n);
-    if (matches.length) {
-      return n;
+  let maxNum = nums.length-1; 
+  while(maxNum >=0){
+  for(let i=0; i<nums.length-maxNum; i++){
+    if(reverse.includes(createCandidate(nums, i, maxNum+i))){
+      return maxNum+i+1-i; 
     }
+  }
+  maxNum=maxNum-1; 
+ }
+ function createCandidate(num,min,max){//creates candidates between ranges of min and max
+   let answer= ""; 
+   answer = answer + num[min].toString(); 
+   for(let c=min+1; c<max+1; c++){
+      answer = answer + "," + num[c].toString(); 
+   }
+   return answer; 
   }
 }
 
